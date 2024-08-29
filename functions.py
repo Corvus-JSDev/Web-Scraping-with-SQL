697797370e79c10a395f717076985b3e4e3ebb15
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-29 14:03:58 -0400

    add header

--- functions.py
+++ functions.py
@@ -1,11 +1,15 @@
 import requests
 import selectorlib
 
+# Pretending to be a browser
+HEADER = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
 def scrape(url):
 	"""Scrape the source page from the given URL"""
 
 	try:
-		response = requests.get(url)
+		response = requests.get(url, HEADER)
 		data = response.text
 		return data
 
